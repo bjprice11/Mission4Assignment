9973542d3055db493260b2551e4a45a7a9b147e0
elifober <eliasfobert@gmail.com>
2026-01-28 15:05:09 -0700

    comments

--- TickTackToe.cs
+++ TickTackToe.cs
@@ -2,8 +2,10 @@ namespace Mission4Assignment;
 
 public class TickTackToe
 {
+    //create an array of 9 positions with a space to display the board dynamically
     private string[] board = new string[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
     
+    //update the array depending on what the player chose
     public bool UpdateBoard(int position, int player)
     {
         if (player == 1)
@@ -13,10 +15,12 @@ public class TickTackToe
             board[position] = "O";
         }
         
+        //call the print board and check winner methods
         PrintBoard(board);
         return CheckWinner(board);
     }
 
+    //print the board based off array position
     public void PrintBoard(string[] board)
     {
         Console.WriteLine($"{board[0]} | {board[1]} | {board[2]}");
@@ -24,14 +28,15 @@ public class TickTackToe
         Console.WriteLine($"{board[3]} | {board[4]} | {board[5]}");
         Console.WriteLine("---------");
         Console.WriteLine($"{board[6]} | {board[7]} | {board[8]}");
-
-        CheckWinner(board);
     }
 
+    //check if the current player won
     public bool CheckWinner(string[] board)
     {
+        //set winner to false until a winner is declared
         bool winner = false;
         
+        //hroizontal winning positions
         if ((board[0] != " " && board[0] == board[1] && board[1] == board[2]) ||
             (board[3] != " " && board[3] == board[4] && board[4] == board[5]) ||
             (board[6] != " " && board[6] == board[7] && board[7] == board[8]))
@@ -39,7 +44,7 @@ public class TickTackToe
             winner = true;
         };
 
-        // Columns
+        // vertical winning positions
         if ((board[0] != " " && board[0] == board[3] && board[3] == board[6]) ||
             (board[1] != " " && board[1] == board[4] && board[4] == board[7]) ||
             (board[2] != " " && board[2] == board[5] && board[5] == board[8]))
@@ -47,14 +52,13 @@ public class TickTackToe
             winner = true;
         }
 
-        // Diagonals
+        // diagonal winner positions
         if ((board[0] != " " && board[0] == board[4] && board[4] == board[8]) ||
             (board[2] != " " && board[2] == board[4] && board[4] == board[6]))
         {
             winner = true;
         }
-        
-
+        //return bool
         return winner;
     }
 }
