c13ce8d28d688e6efebab031e837c496074f2418
bjprice11 <bjprice110@gmail.com>
2026-01-28 14:16:18 -0700

    Program.cs updated to allow for two players to enter the position they want and see if they won.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,57 @@
 ﻿
+using Mission4Assignment;
+
+TickTackToe tt = new TickTackToe();
+
+Console.WriteLine("Welcome to the TickTacToe Game");
+List<int> userChoices = new List<int>();
+bool winner = false;
+string playerOne = "X";
+string playerTwo = "O";
+
+do
+{
+    int playerOneChoice = -1;
+    Console.WriteLine("Player One: Which position would you like to play? 1-9");
+    playerOneChoice = (Convert.ToInt32(Console.ReadLine())-1);
+    if  (userChoices.Contains(playerOneChoice))
+    {
+        Console.WriteLine("That position is already taken, Choose again!");
+    }
+    else
+    {
+        userChoices.Add(playerOneChoice);
+    }
+    Console.WriteLine(tt.UpdateBoard(playerOneChoice, playerOne));
+
+    if (tt.CheckWinner(playerOne))
+    {
+        Console.WriteLine("Player One Wins!");
+        winner = true;
+        break; // Break the main game loop
+    }
+    
+    int playerTwoChoice = -1;
+    Console.WriteLine("Player Two: Which position would you like to play? 1-9");
+    playerTwoChoice = (Convert.ToInt32(Console.ReadLine())-1);
+    
+    if  (userChoices.Contains(playerTwoChoice))
+    {
+        Console.WriteLine("That position is already taken, Choose again!");
+    }
+    else
+    {
+        userChoices.Add(playerTwoChoice);
+    }
+    Console.WriteLine(tt.UpdateBoard(playerTwoChoice, playerTwo));
+    
+    if (tt.CheckWinner(playerTwo))
+    {
+        Console.WriteLine("Player Two Wins!");
+        winner = true;
+        break;
+    };
+    
+
+}while (!winner);
+Console.WriteLine("Thanks for playing!");
